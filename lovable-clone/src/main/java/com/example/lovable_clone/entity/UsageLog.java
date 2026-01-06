7f1330acf837d1d76769e2a33f6776580f2c66b4
Kaustubh <kaustubhbhatt18@gmail.com>
2026-01-06 19:14:46 +0530

    Added all the entity and enums files in the project

---
+++ lovable-clone/src/main/java/com/example/lovable_clone/entity/UsageLog.java
@@ -0,0 +1,14 @@
+package com.example.lovable_clone.entity;
+
+import java.time.Instant;
+
+public class UsageLog {
+    Long id;
+    User user;
+    Project project;
+    String action;
+    Integer tokensUsed;
+    Integer durationMs;
+    String metaData;//Json of{model_used,prompt_used},
+    Instant createdAt;
+}
