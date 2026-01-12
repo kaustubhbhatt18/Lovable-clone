80ef32c9641854182219615d4826648669c7bcfd
Kaustubh <kaustubhbhatt18@gmail.com>
2026-01-12 20:30:40 +0530

    Connected to database successfully

---
+++ lovable-clone/src/main/java/com/example/lovable_clone/Service/impl/FileServiceImpl.java
@@ -0,0 +1,20 @@
+package com.example.lovable_clone.Service.impl;
+
+import com.example.lovable_clone.Service.FileService;
+import com.example.lovable_clone.dto.project.FileContentResponse;
+import com.example.lovable_clone.dto.project.FileNode;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
+public class FileServiceImpl implements FileService {
+    @Override
+    public List<FileNode> getFileTree(Long projectId, Long userId) {
+        return List.of();
+    }
+
+    @Override
+    public FileContentResponse getFileContent(Long projectId, String path, Long userId) {
+        return null;
+    }
+}
