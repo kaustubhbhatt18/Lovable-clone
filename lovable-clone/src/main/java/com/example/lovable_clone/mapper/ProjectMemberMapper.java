119d1b90f82c7d44ac2685b6d9222857f7f19e19
Kaustubh <kaustubhbhatt18@gmail.com>
2026-01-22 15:46:40 +0530

    Added a function in ProjectMemberServiceImpl

---
+++ lovable-clone/src/main/java/com/example/lovable_clone/mapper/ProjectMemberMapper.java
@@ -0,0 +1,4 @@
+package com.example.lovable_clone.mapper;
+
+public interface ProjectMemberMapper {
+}
---
+++ lovable-clone/src/main/java/com/example/lovable_clone/repository/ProjectMemberRepository.java
@@ -0,0 +1,4 @@
+package com.example.lovable_clone.repository;
+
+public interface ProjectMemberRepository {
+}
